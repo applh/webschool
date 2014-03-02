239f42057fcdcba1ba7f6aa347e2c74182fb0e41
Applh.com <github@applh.com>
2014-03-02 20:35:07 +0100

    add basic walk animation
    * walk E, W, SE, SW
    * add dark color for background layer

--- hellios/armanoid.js
+++ hellios/armanoid.js
@@ -14,15 +14,44 @@ ARM.jinit=function() {
 
 ARM.act_anim1=function(){
 	ARM.info("A1");
+	$(".armanoid .left").animate({"left": "+=10px"});
+	$(".armanoid .mul1").animate({"left": "+=10px"});
+	$(".armanoid .mul2").animate({"left": "+=20px"});
+	$(".armanoid .mul3").animate({"left": "+=30px"});
+	$(".armanoid .mll").animate({"left": "+=10px"});
+	
+	$(".armanoid .right").animate({"left": "-=10px"});
+	$(".armanoid .mur1").animate({"left": "-=10px"});	
+	$(".armanoid .mur2").animate({"left": "-=20px"});	
+	$(".armanoid .mur3").animate({"left": "-=30px"});	
+	$(".armanoid .mlr").animate({"left": "-=10px"});	
 };
 ARM.act_anim2=function(){
 	ARM.info("A2");
+	$(".armanoid .left").animate({"left": "-=10px"});
+	$(".armanoid .mul1").animate({"left": "-=10px"});
+	$(".armanoid .mul2").animate({"left": "-=20px"});
+	$(".armanoid .mul3").animate({"left": "-=30px"});
+	$(".armanoid .mll").animate({"left": "-=10px"});
+
+	$(".armanoid .right").animate({"left": "+=10px"});
+	$(".armanoid .mur1").animate({"left": "+=10px"});
+	$(".armanoid .mur2").animate({"left": "+=20px"});
+	$(".armanoid .mur3").animate({"left": "+=30px"});
+	$(".armanoid .mlr").animate({"left": "+=10px"});
 };
 ARM.act_anim3=function(){
 	ARM.info("A3");
+	$(".left").css("z-index", 10).css("background-color", "#bbbbbb");
+	$(".center").css("z-index", 20).css("background-color", "#dddddd");
+	$(".right").css("z-index", 30).css("background-color", "#eeeeee");
+	
 };
 ARM.act_anim4=function(){
 	ARM.info("A4");
+	$(".left").css("z-index", 30).css("background-color", "#eeeeee");
+	$(".center").css("z-index", 20).css("background-color", "#dddddd");
+	$(".right").css("z-index", 10).css("background-color", "#bbbbbb");
 };
 ARM.act_anim5=function(){
 	ARM.info("A5");
