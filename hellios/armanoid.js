4e2fe89f4aa467c52a23c474a2969667f80c634d
Applh.com <github@applh.com>
2014-03-02 21:16:35 +0100

    add eyes
    * eyes looking left/right
    * improvement on arms and hands

--- hellios/armanoid.js
+++ hellios/armanoid.js
@@ -42,16 +42,38 @@ ARM.act_anim2=function(){
 };
 ARM.act_anim3=function(){
 	ARM.info("A3");
+	// LOOK EST
 	$(".left").css("z-index", 10).css("background-color", "#bbbbbb");
 	$(".center").css("z-index", 20).css("background-color", "#dddddd");
 	$(".right").css("z-index", 30).css("background-color", "#eeeeee");
+
+	$(".mur3").css("z-index", 40).css("background-color", "#ffffff");
 	
+	$(".abdos")
+		.css("border-bottom-right-radius", "0px")
+		.css("border-bottom-left-radius", "32px");
+	$(".head")
+		.css("border-top-right-radius", "0px")
+		.css("border-top-left-radius", "16px");
+	$(".eyes").css("float", "left");	
 };
 ARM.act_anim4=function(){
 	ARM.info("A4");
+	// LOOK WEST
 	$(".left").css("z-index", 30).css("background-color", "#eeeeee");
 	$(".center").css("z-index", 20).css("background-color", "#dddddd");
 	$(".right").css("z-index", 10).css("background-color", "#bbbbbb");
+	
+	$(".mul3").css("z-index", 40).css("background-color", "#ffffff");
+	
+	$(".abdos")
+		.css("border-bottom-right-radius", "32px")
+		.css("border-bottom-left-radius", "0px");
+	$(".head")
+		.css("border-top-right-radius", "16px")
+		.css("border-top-left-radius", "0px");
+	$(".eyes").css("float", "right");	
+	
 };
 ARM.act_anim5=function(){
 	ARM.info("A5");
